4b8596f34d87aebecb1ab9c1afdd633d680b3994
Kazi Arafat <arafatkazi2448@gmail.com>
2022-10-19 20:30:02 +0000

    basic setup

---
+++ app.ts
@@ -0,0 +1,13 @@
+import express,{Express,Request,Response} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app:Express = express();
+const port = process.env.PORT;
+
+app.get('/',(req:Request,res:Response)=>{
+    res.send("Node~~Express~~Typescript app response");
+});
+
+app.listen(port,() => console.log(`Server is running at ${port}`));
